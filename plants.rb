bc777523e909c68801855023b33cf6d6d5de2155
Yain Porter <yainporter@gmail.com>
2023-09-20 10:07:44 -0700

    Add plants.rb code

--- plants.rb
+++ plants.rb
@@ -3,6 +3,8 @@ ounches_of_water = 10
 
 if gets_sunshine == true && ounches_of_water >= 10
     puts "Your plant will thrive"
+elsif gets_sunshine == true || ounches_of_water >= 10
+    puts "Your plant will be okay"
 else
     puts "Your plant will die"
 end
